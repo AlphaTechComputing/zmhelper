28e03ea19c59e2b7cfeaae86d12fcadd69783591
harrypit06 <mkennedy@alphatech-computing.com>
2017-08-17 11:29:29 -0400

    Added logging and updated installer.

--- checkzm.py
+++ checkzm.py
@@ -3,14 +3,23 @@
 import commands
 import time
 import os
+import logging
+from time import gmtime, strftime
 
 #find process
 pc = commands.getoutput('ps -aux | grep zmwatch.pl | grep -v grep').strip()
+time_format = "%Y-%m-%d %H:%M:%S"
+
+logging.basicConfig(filename='/var/log/checkzm.log',level=logging.DEBUG)
 
 if(len(pc) > 0):
-          print 'zoneminder running. skip'
+	print 'zoneminder running. skip'
+	log_time = strftime(time_format, time.localtime()) + ':'
+	logging.info(log_time + 'zoneminder running.')
+
 
 else:
-	  print 'zoneminder not running'
-          os.system('sudo service zoneminder start')
-  	  print 'zoneminder started'
+	log_time = strftime(time_format, time.localtime()) + ':'
+        logging.warning(log_time + 'zoneminder not running, starting.')
+        os.system('sudo service zoneminder start')
+  	print 'zoneminder not running, starting.'
--- installhelper.py
+++ installhelper.py
@@ -13,3 +13,6 @@ job.minute.every(1)
 cron.write()
 
 os.system('sudo crontab -l')
+
+with open("/etc/logrotate.conf", "a") as myfile:
+  myfile.write("\n" + "/var/log/checkzm.log {" + "\n\t monthly \n\t size 100M \n\t compress \n\t delaycompress \n\t missingok \n\t notifempty \n\t create 644 root root \n }")
